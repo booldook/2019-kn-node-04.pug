7fdabf568c62be4af65a75af90659451ce81a4dd
hee20192 <html20192@daum.net>
2019-10-27 15:24:21 +0900

    user.js

---
+++ public/js/user.js
@@ -0,0 +1,19 @@
+$.ajax({
+	url: "/api/list",
+	type: "get",
+	dataType: "json",
+	success: function(res) {
+		html = '';
+		for(var i in res.result) {
+			html += '<tr>';
+			html += '<td>'+res.result[i].id+'</td>';
+			html += '<td>'+res.result[i].name+'</td>';
+			html += '<td>'+res.result[i].age+'</td>';
+			html += '</tr>';
+		}
+		$(".table > tbody").append(html);
+	},
+	error: function(xhr, status, error) {
+		console.log(xhr);
+	}
+});
